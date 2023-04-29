aedaf00df5ec8040c4b0328a88c483f93eac735a
MaksymKryvohus <108764846+MaksymKryvohus@users.noreply.github.com>
2023-04-29 16:50:16 +0300

    домашка

--- js/task-01.js
+++ js/task-01.js
@@ -1,4 +1,4 @@
-const categoriesList = document.querySelector('#categories');
+const categoriesList = document.getElementById('categories');
 const categoriesItems = categoriesList.querySelectorAll('li.item');
 
 console.log(`Кількість категорій: ${categoriesItems.length}`);
--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,26 @@
+const form = document.querySelector('.login-form');
+
+form.addEventListener('submit', event => {
+    event.preventDefault();
+
+    const emailInput = form.elements.email;
+    const passwordInput = form.elements.password;
+
+    if (emailInput.value === '' || passwordInput.value === '') {
+        alert('Потрібно заповнити всі поля');
+        return;
+    }
+
+    const formData = {
+        email: emailInput.value,
+        password: passwordInput.value,
+    };
+
+    console.log(formData);
+    form.reset();
+});
+
+
+
+
 
